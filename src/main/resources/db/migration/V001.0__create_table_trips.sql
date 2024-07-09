b0130aaa118da67614e637d8110262409d2a4911
Robson Barbosa Souza <robson.souza@olgari.com.br>
2024-07-08 23:07:20 -0300

    chore: setup database

---
+++ src/main/resources/db/migration/V001.0__create_table_trips.sql
@@ -0,0 +1,10 @@
+CREATE TABLE trips
+(
+    id           uuid                  DEFAULT random_uuid() PRIMARY KEY,
+    destination  VARCHAR(255) NOT NULL,
+    starts_at    TIMESTAMP    NOT NULL,
+    ends_at      TIMESTAMP    NOT NULL,
+    is_confirmed BOOLEAN      NOT NULL DEFAULT FALSE,
+    owner_name   VARCHAR(255) NOT NULL,
+    owner_email  VARCHAR(255) NOT NULL
+);
