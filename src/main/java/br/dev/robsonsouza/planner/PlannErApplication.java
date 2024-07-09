7766877f7f1bb0552995a87150b753e5253378db
Robson Barbosa Souza <robson.souza@olgari.com.br>
2024-07-08 21:24:50 -0300

    chore: start project NLW 16 by Rocketseat

---
+++ src/main/java/br/dev/robsonsouza/planner/PlannErApplication.java
@@ -0,0 +1,12 @@
+package br.dev.robsonsouza.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannErApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(PlannErApplication.class, args);
+    }
+}
---
+++ src/test/java/br/dev/robsonsouza/planner/PlannErApplicationTests.java
@@ -0,0 +1,12 @@
+package br.dev.robsonsouza.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannErApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+}
